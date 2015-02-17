045f7826951cce311841a34bf1b94359fc7b6389
jonchang03 <jonchang03@gmail.com>
2015-02-17 15:52:18 -0500

    remove #define and struct from sock352lib.c

--- sock352lib.c
+++ sock352lib.c
@@ -1,26 +1,5 @@
 #include <sock352lib.h>
 
-#define CLOSED 1
-#define SYN_SENT 2
-#define ESTABLISHED 3
-#define FIN_WAIT_1 4
-#define FIN_WAIT_2 5
-#define TIME_WAIT 6
-#define LISTEN 7
-#define SYN_RCVD 8
-#define CLOSE_WAIT 9
-#define LAST_ACK 10
-
-typedef struct cs352_connection {
-  
-}
-
-typedef struct cs352_fragment { 
-  unsigned int state;
-  unsigned int portNum;
-}fragment;
-
-
 int sock352_init(int udp_port)
 {
   if (udp_port != 0) {
