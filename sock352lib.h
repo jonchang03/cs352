90a4b403ce6de2f59b52d3fc7ebd0aec0ee3bf50
Shihang Zhang <zhshihang@gmail.com>
2015-02-25 17:33:02 -0500

    change to hash table

--- sock352lib.h
+++ sock352lib.h
@@ -1,10 +1,12 @@
 #ifndef SOCK352LIB_H
-#def SOCK352LIB_H
+#define SOCK352LIB_H 1
 
-#include <sock352.h>
+#include "uthash.h"
+#include "utlist.h"
 #include <stdlib.h>
 #include <pthread.h>
 #include <stdint.h>
+#include "sock352.h"
 
 #define CLOSED 1
 #define SYN_SENT 2
@@ -18,36 +20,30 @@
 #define LAST_ACK 10
 
 typedef struct sock352_connection {
+  uint32_t state;
 
-  uint32_t sock352_fd;                    /* file descriptor */
-  uint32_t state;                         /* holds state of the connection (open, connected, waiting) */
+  pthread_mutex_t lock;          /* mutex locks to access the connection */
+  struct sock352_fragment * fragments;     /* list of fragments */
+  struct sock352_connection *next;
+  struct sock352_connection *prev;
 
-  pthread_mutex_t lock;                   /* mutex locks to access the connection */
+  unsigned int source_port;
+  unsigned int dest_port;
+  unsigned int next_seqNum;
+  struct sock352_fragment *noAckButTransmt_frags;
+  struct sock352_fragment *recvd_frags;
 
-  struct sock352_fragment * fragments;    /* list of fragments */
-  struct sock352_connection *next;        /* list of connections */
-  struct sock352_connection *prev;  
-  
-  uint32_t local_port;                    /* sockets for local and remote ports */
-  uint32_t remote_port;
-  uint16_t src_port;                      /* source and destination UDP ports */
-  uint16_t dest_port;
 
-  uint64_t timeout;
-  uint64_t ACK;                           /* sent and acknowledged */
-  uint64_t UNACK;                         /* sent and unacknowledged */
-  uint64_t MAX;                           /* maximum we can send */ 
-
+  unsigned int sock352_fd;
+  UT_hash_handle hh;
 }sock352_connection_t;
 
 typedef struct sock352_fragment {
-  struct sock352_connection *connection;  /* connection for the fragment */
-  uint32_t size;                          /* size of the fragment in bytes */
-  uint32_t next;                          /* index for next data */
-  uint64_t seq_start;                     /* start sequence number */
-  uint64_t timestamp;                     /* time sent or received */
-
-  struct sock352_fragment *next;          /* list of fragments for each connection */
+  struct sock352_connection *connection;
+  unsigned int size;
+  sock352_pkt_hdr_t header;
+  char data[MAXIMUM_LENGTH];
+  struct sock352_fragment *next;
   struct sock352_fragment *prev;
 }sock352_fragment_t;
 
